898e03eae8e4d6f9d61dca51e3ba0ca4047dfb14
Vishwas904 <118361500+Vishwas904@users.noreply.github.com>
2023-02-18 15:30:51 +0530

    Add files via upload

---
+++ Sort.js
@@ -0,0 +1,13 @@
+//Sorting the array in Ascending Order using the sort method 
+//Ascendong Order
+let compare = (a, b) => {
+    return a-b
+  }
+  console.log(line.sort(compare))
+
+  //Descending Order
+//   let compare = (a, b) => {
+//     return b-a
+//   }
+//   console.log(line.sort(compare))
+
